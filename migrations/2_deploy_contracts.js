61e312506499e3cde4d671542cdfddc8b85cb781
LucaCellamare <lucagiuseppe.cellamare@mail.polimi.it>
2021-11-24 02:00:56 -0600

    added tests for submitChangeSpenderRequest and submitTransaction

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 var SocialRecoveryWallet= artifacts.require("SocialRecoveryWallet");
 
 module.exports = function(deployer) {
-    const guardians = ["0x24d6fba577827a018a8a666eb04f1f0eb30559848ae51ceda426589d467e4527", "0x85c65152356d9cd1e88049680e1ebeeb4f7c3c89632795446af7fbe667b306e0"]; // 0x502... 0xf89...
-    deployer.deploy(SocialRecoveryWallet,"0x96121a4f513ab387f4fb60555d9940d54fd9c710", guardians, [], 1,1,1);
+    const guardians = ["0xf45df42b5796d1f462c88b50c55f3e5b3e8b8146b314cb6bdb8035d9446e0074", "0xa2047c42436e0fc4f226985e4ae3850e58ce802cfdf1aeae76b447e9b1b17c87"]; // 0x502... 0xf89...
+    deployer.deploy(SocialRecoveryWallet,"0x9e4cbb7be000e2be521092811ce79555618b1c29", guardians, [], 1,2,1);
 };
--- test/socialRecoveryWallet.js
+++ test/socialRecoveryWallet.js
@@ -4,13 +4,86 @@ contract("SocialRecoveryWalletDeployment", async accounts => {
 
   it('Should deploy smart contract properly', async () => {
     const socialRecoveryWalletNew = await SocialRecoveryWallet.deployed();
-    console.log(socialRecoveryWalletNew.address);
     assert(socialRecoveryWalletNew.address != '');
   });
 
+  it('Should fail when changeSpenderRequest is not called by a Guardian', async () => {
+    const socialRecoveryWalletNew = await SocialRecoveryWallet.deployed();
+    try{
+      await socialRecoveryWalletNew.submitChangeSpenderRequest("0x5186b30bf4c723fdc6aad80cdbfe95a0efc33261",{from: "0x9e4cbb7be000e2be521092811ce79555618b1c29"});
+    }catch(e){
+      assert(e.reason == 'not guardian')
+    }
+    assert((await socialRecoveryWalletNew.spender.call()).toLowerCase() != "0x5186b30bf4c723fdc6aad80cdbfe95a0efc33261");
+  });
+
+
   it('Should change spender correctly when changeSpenderRequest is called by a Guardian and threshold is respected', async () => {
     const socialRecoveryWalletNew = await SocialRecoveryWallet.deployed();
-    await socialRecoveryWalletNew.submitChangeSpenderRequest("0x2134f44b5d3d34f6fc35f47604aca51314c8e3a5", { from: "0xf8917156d89939248bc14fa3f1066f3dc64b29e1" });
-    assert((await socialRecoveryWalletNew.spender.call()).toLowerCase() == "0x2134f44b5d3d34f6fc35f47604aca51314c8e3a5".toLowerCase());
+    await socialRecoveryWalletNew.submitChangeSpenderRequest("0x5186b30bf4c723fdc6aad80cdbfe95a0efc33261", { from: "0xafd66fa7dc129ce79cb04456821534d6d2c9d52c" });
+    try{
+      await socialRecoveryWalletNew.confirmChangeSpenderRequest(0, { from: "0xa432cf8da8be9172dc59ea53ea386966837e0c2a" });
+    }catch(e){
+      assert(e.reason == 'not guardian')
+    }
+    assert((await socialRecoveryWalletNew.spender.call()).toLowerCase() == "0x5186b30bf4c723fdc6aad80cdbfe95a0efc33261");
   });
+
+
+  it('Should not change spender when changeSpenderRequest is called by a Guardian but threshold is not respected', async () => {
+    const socialRecoveryWalletNew = await SocialRecoveryWallet.deployed();
+    await socialRecoveryWalletNew.submitChangeSpenderRequest("0x51d36bc4670fc6035b3b700abf904ff8b279405e", { from: "0xafd66fa7dc129ce79cb04456821534d6d2c9d52c" });
+    assert((await socialRecoveryWalletNew.spender.call()).toLowerCase() != "0x51d36bc4670fc6035b3b700abf904ff8b279405e");
+  });
+
+  it('Should fail when submitTransaction is not called by the spender', async () => {
+    const socialRecoveryWalletNew = await SocialRecoveryWallet.deployed();
+    let before_balance = await web3.eth.getBalance(socialRecoveryWalletNew.address)
+    try{
+      await socialRecoveryWalletNew.submitTransaction("0x51d36bc4670fc6035b3b700abf904ff8b279405e", 10000000000, '0x',{from: "0x51d36bc4670fc6035b3b700abf904ff8b279405e"});
+    }catch(e){
+      assert(e.reason == 'not spender')
+    }
+    assert((await web3.eth.getBalance(socialRecoveryWalletNew.address)) == before_balance);
+  });
+  
+  it('Should fail when submitTransaction is called by the spender but is not confirmed by the guardians', async () => {
+    const socialRecoveryWalletNew = await SocialRecoveryWallet.deployed();
+    
+    let accounts = await web3.eth.getAccounts();
+    await web3.eth.sendTransaction({to:socialRecoveryWalletNew.address, from:accounts[0], value: web3.utils.toWei('1')});
+    
+    let before_balance = await web3.eth.getBalance(socialRecoveryWalletNew.address);
+    
+    await socialRecoveryWalletNew.submitTransaction("0x51d36bc4670fc6035b3b700abf904ff8b279405e", 10000000000, '0x',{from: "0x5186b30bf4c723fdc6aad80cdbfe95a0efc33261"});
+    try{
+      await socialRecoveryWalletNew.executeTransaction(0,{from: "0x5186b30bf4c723fdc6aad80cdbfe95a0efc33261"});
+    }catch(e){
+      assert(e.reason == 'cannot execute tx')
+    }
+    assert((await web3.eth.getBalance(socialRecoveryWalletNew.address)) == before_balance);
+  });
+
+
+
+  it('Should execute transaction when submitTransaction is called by the spender and is confirmed by the guardians', async () => {
+    const socialRecoveryWalletNew = await SocialRecoveryWallet.deployed();
+    
+    let accounts = await web3.eth.getAccounts();
+    let before_balance = await web3.eth.getBalance(socialRecoveryWalletNew.address);
+    console.log(before_balance);
+    
+    await socialRecoveryWalletNew.submitTransaction("0x51d36bc4670fc6035b3b700abf904ff8b279405e", 10000000000, '0x',{from: "0x5186b30bf4c723fdc6aad80cdbfe95a0efc33261"});
+    await socialRecoveryWalletNew.confirmTransaction(1,{from: "0xafd66fa7dc129ce79cb04456821534d6d2c9d52c"});
+
+    try{
+      await socialRecoveryWalletNew.executeTransaction(1,{from: "0x5186b30bf4c723fdc6aad80cdbfe95a0efc33261"});
+    }catch(e){
+      console.log(e.reason);
+      assert(e.reason == 'cannot execute tx')
+    }
+    assert((await web3.eth.getBalance(socialRecoveryWalletNew.address)) == before_balance-10000000000);
+  });
+
+
 });
